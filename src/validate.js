6419649e6ca5749ceb11761b33d53df0697ade08
Joseph Mearman <joseph@mearman.co.uk>
2020-12-03 12:52:42 +0000

    feat: per-version uuid validation
    adds specific validation for all uuid versions
    should be 100% backward compatible

--- src/validate.js
+++ src/validate.js
@@ -1,7 +1,26 @@
-import REGEX from './regex.js';
+import { genericUuid, v1, v3, v4, v5 } from './regex.js';
 
-function validate(uuid) {
-  return typeof uuid === 'string' && REGEX.test(uuid);
+function validateByVersion(uuid, expression) {
+  return typeof uuid === 'string' && expression.test(uuid);
 }
 
-export default validate;
+function isUuid(uuid) {
+  return validateByVersion(uuid, genericUuid);
+}
+export default isUuid;
+
+export function isV1(uuid) {
+  return validateByVersion(uuid, v1);
+}
+
+export function isV3(uuid) {
+  return validateByVersion(uuid, v3);
+}
+
+export function isV4(uuid) {
+  return validateByVersion(uuid, v4);
+}
+
+export function isV5(uuid) {
+  return validateByVersion(uuid, v5);
+}
